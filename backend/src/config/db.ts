b9ae6e5c5717ca62ee3d798ac0e5cf8c9e640f97
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2025-01-13 11:51:45 +0545

    fixed mongo db uri

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,15 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import { MONGO_URI } from "./config";
 
-const connectDatabase = async()=>{
-try {
-    await mongoose.connect(MONGO_URI);
-    console.log("Mongodb connected successfully")
-} catch (error) {
-    console.log("MongoDb connection failed", error)
-    process.exit(1)
-}
-
-}
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://moonergone0:jpRdZ1BLRkWNr8sN@cluster0.5esvt.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Mongodb connected successfully");
+  } catch (error) {
+    console.log("MongoDb connection failed", error);
+    process.exit(1);
+  }
+};
 export default connectDatabase;
